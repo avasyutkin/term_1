721b2e6e6757a4623ecb3464fdfcd9b1dbab27ff
Alexandr Vasuytkin <hadigi7557@gmail.com>
2018-12-29 11:42:19 +0300

    Lab

--- My labs/Lab_13/encryption.cpp
+++ My labs/Lab_13/encryption.cpp
@@ -1,5 +1,3 @@
-//OpenSSL
-//#include <stdio.h>
 #include <iostream>
 #include <openssl/conf.h> // функции, структуры и константы настройки OpenSSL
 #include <openssl/conf.h>
@@ -8,15 +6,14 @@
 #include <openssl/aes.h>
 #include <fstream>
 
-#include <openssl/rsa.h> // Алгоритм RSA
-#include <openssl/pem.h> // Для работы с файлами ключей
-
 #pragma comment (lib, "ws2_32.LIB")
 #pragma comment (lib, "gdi32.LIB")
 #pragma comment (lib, "advapi32.LIB")
 #pragma comment (lib, "crypt32")
 #pragma comment (lib, "user32")
 #pragma comment (lib, "wldap32")
+//
+//extern "C" _declspec(dllimport) int RSA_public_encrypt(int flen, const unsigned char *from, const unsigned char *to, RSA *rsa, int padding);
 
 // библиотеки OpenSSL (openssl.org) подключаются неявно динамически (см. конспект лаб. по библиотекам)
 
@@ -24,26 +21,38 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
 	// работа с криптофункциями OpenSSL:
 	// 1) создание объекта с настройками
-	// 2) собственно, шифрование
+	// 2) собственно, шифрование	
 	// 3) финализация
 	// 4) и вывод зашифрованных данных
 
 	// как правило, в литературе, структуры используются для хранения только данных
-	// ни слова о методах и конструкторах/деструкторах
+	// ни слова о методах и конструкторах/деструкторах 
+	char D[255];
+	fstream fd;
+	fstream fe;
+	fstream fS;
 
-	char inputtext[256];
-
-	int cryptedtext_len;
-	int len;
-	unsigned char *plaintext =
-		(unsigned char *) "EVP_EncryptUpdate() encrypts inl bytes from the buffer in and writes the encrypted version to out"; // исходный текст
+	unsigned char *plaintext = /*(unsigned char *)D;*/
+		(unsigned char *)"EVP_EncryptUpdate() encrypts inl bytes from the buffer in and writes the encrypted version to out";// исходный текст
 	int plaintext_len = strlen((char *)plaintext); // длина текста
 	unsigned char *key = (unsigned char *)"0123456789"; // пароль (ключ)
 	unsigned char *iv = (unsigned char *)"0123456789012345"; // инициализирующий вектор, рандомайзер
 	unsigned char cryptedtext[256]; // зашифрованный результат
 	unsigned char decryptedtext[256]; // расшифрованный результат
+
+	//  
+
+
+	//
+	//открыть файл в бинарном виде 
+	//
+	//
+	//
+	//f0.open("f0.txt", std::fstream::in | std::fstream::binary); // файл с исходными данными
+	//
 
 	// 1. Создаётся указатель на несуществующую структуру
 	// структура - тип данных в C++, близка к КЛАССУ, различия минимальны
@@ -58,42 +67,63 @@ int main()
 		NULL,
 		key, // ключ/пароль/секрет
 		iv); // рандомайзер (случайный начальный вектор)
-	
-	std::ifstream inputfile("inputtext.txt");
-	while (!inputfile.eof())
+
+	// 4. САМ ПРОЦЕСС ШИФРОВАНИЯ - ФУКНЦИЯ EVP_EncryptUpdate
+
+	char str[256];
+	int len = 0;
+	int cryptedtext_len = len;
+	ifstream input("input.txt", std::fstream::binary);
+	std::fstream output;
+	output.open("out.txt", std::fstream::out | std::fstream::in | std::fstream::trunc | std::fstream::binary);
+
+	input.read(str, 256);
+	while (input.gcount() > 0)
+
 	{
-		//inputfile.open(inputtext, 256, ios::in);
-		inputfile.get(inputtext, 256);
-		inputtext >> plaintext;
-		
 
 
-		// 4. САМ ПРОЦЕСС ШИФРОВАНИЯ - ФУКНЦИЯ EVP_EncryptUpdate
+
+
 		EVP_EncryptUpdate(ctx, // объект с настройками
 			cryptedtext, // входной параметр: ссылка, куда помещать зашифрованные данные
 			&len, // выходной параметр: длина полученного шифра
-			plaintext, // входной параметр: что шифровать
-			plaintext_len); // входной параметр : длина входных данных
-			cryptedtext_len = len;
+			(unsigned char *)str, input.gcount());
+		// входной параметр: что шифровать
+		// входной параметр : длина входных данных
+		cryptedtext_len = len;
+		output.write((char *)cryptedtext, len);
+		input.read(str, 256);
 
-		// 5. Финализация процесса шифрования
-		// необходима, если последний блок заполнен данными не полностью
-		EVP_EncryptFinal_ex(ctx, cryptedtext + len, &len);
-		cryptedtext_len += len;
 	}
+
+
+
+	// 5. Финализация процесса шифрования
+	// необходима, если последний блок заполнен данными не полностью
+	EVP_EncryptFinal_ex(ctx, cryptedtext, &len);
+	cryptedtext_len += len;
+	output.write((char *)cryptedtext, len);
+	input.close();
+
 	// 6. Удаление структуры
 	EVP_CIPHER_CTX_free(ctx);
+	output.close();
 
 	// вывод зашифрованных данных
+	//for (int i = 0; i < cryptedtext_len; i++)
+	//{
+	//	cout << hex << cryptedtext[i];
+	//	if ((i + 1) % 32 == 0) cout << endl;
+	//	
+	//}
+	//fS << D << "привет";
 
-	for (int i = 0; i < cryptedtext_len; i++)
-	//while ()
-	{
-		std::ofstream fout("crypt.txt");
-		fout << hex << cryptedtext[i];
-		if ((i + 1) % 32 == 0) cout << endl;
-	}
-	cout << endl;
+	//cout << endl;
+
+
+
+
 
 	// РАСШИФРОВКА
 
@@ -102,19 +132,31 @@ int main()
 
 	// 2.
 	EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv); // инициализация методом AES, ключом и вектором
+	std::fstream deshef_input;
+	deshef_input.open("‎⁨out.txt", std::fstream::in | std::fstream::binary);
+	std::fstream deshef_output;
+	deshef_output.open("‎⁨deasd_output.txt", std::fstream::out | std::fstream::in | std::fstream::trunc | std::fstream::binary);
+	char str2[256];
+	deshef_input.read(str2, 256);
+	while (deshef_input.gcount() > 0)
+	{
+		EVP_DecryptUpdate(ctx,
+			decryptedtext,
+			&len,
+			(unsigned char *)str2,
+			deshef_input.gcount());  // СОБСТВЕННО, ШИФРОВАНИЕ
 
-	// 3.
-	EVP_DecryptUpdate(ctx, decryptedtext, &len, cryptedtext, cryptedtext_len);  // СОБСТВЕННО, ШИФРОВАНИЕ
+		cryptedtext_len = len;
+		deshef_output.write((char *)decryptedtext, len);
+		deshef_input.read(str2, 256);
+	}
+	EVP_DecryptFinal_ex(ctx, decryptedtext, &len);
 
-	// 4.
-	int dectypted_len = len;
-	EVP_DecryptFinal_ex(ctx, decryptedtext + len, &len);
-
-	// 5.
-	dectypted_len += len;
 	EVP_CIPHER_CTX_free(ctx);
-	decryptedtext[dectypted_len] = '\0';
-	cout << decryptedtext << endl;
+	deshef_output.write((char*)decryptedtext, len);
+	deshef_output.close();
+	deshef_input.close();
+
 
 	// --- шифрование файла
 	// производится точно так же, но порциями, в цикле
@@ -133,5 +175,5 @@ int main()
 		9) закрытие файлов
 	*/
 	getchar();
-    return 0;
+	return 0;
 }
