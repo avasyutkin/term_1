073901daeff6b00819aa3066fbf616a4df3128f3
Alexander <43503189+san7557@users.noreply.github.com>
2019-02-27 16:36:35 +0300

    Delete func.cpp

--- vasyutkin_term2/VisualStudio/181_331_vasyutkin/quote/func.cpp
+++
@@ -1,9 +0,0 @@
-#include "quote.h"
-#include <string>
-
-using namespace std;
-
-string Quote::concatenation(Quote t) //вынесли метод за пределы класса
-{
-	return this->_content + t.getContent();
-}
